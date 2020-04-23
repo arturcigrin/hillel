3b9dce9b88f5bc1474a23a8174309119e3b0476b
Chigrin Artur <arturcigrin@gmail.com>
2020-04-23 17:40:06 +0300

    Дополнительное задание

--- app.js
+++ app.js
@@ -6,33 +6,51 @@ const operation = prompt(
   / - деление.
   ** - возведение в степень.
   max - найти максимальное число.
-  `
+  cos - найти косинус числа.
+  sin - найти синус числа.
+  tan - найти тангенс числа.
+ `
 );
 
-const firstNumber = +prompt("Введите первое число.");
-const secondNumber = +prompt("Введите второе число.");
+if (operation === "cos" || operation === "sin" || operation === "tan") {
+  const number = +prompt("Введите число.");
 
-switch (operation) {
-  case "+":
-    additionOfNumbers(firstNumber, secondNumber);
-    break;
-  case "-":
-    subtractionOfNumbers(firstNumber, secondNumber);
-    break;
-  case "*":
-    multiplicationOfNumbers(firstNumber, secondNumber);
-    break;
-  case "/":
-    divisionOfNumbers(firstNumber, secondNumber);
-    break;
-  case "**":
-    powOfNumbers(firstNumber, secondNumber);
-    break;
-  case "max":
-    maxOfNumbers(firstNumber, secondNumber);
-    break;
-  default:
-    alert("С таким оператором нет действия.");
+  switch (operation) {
+    case "cos":
+      cosOfNumber(number);
+      break;
+    case "sin":
+      sinOfNumber(number);
+      break;
+    case "tan":
+      tanOfNumber(number);
+  }
+} else {
+  const firstNumber = +prompt("Введите первое число.");
+  const secondNumber = +prompt("Введите второе число.");
+
+  switch (operation) {
+    case "+":
+      additionOfNumbers(firstNumber, secondNumber);
+      break;
+    case "-":
+      subtractionOfNumbers(firstNumber, secondNumber);
+      break;
+    case "*":
+      multiplicationOfNumbers(firstNumber, secondNumber);
+      break;
+    case "/":
+      divisionOfNumbers(firstNumber, secondNumber);
+      break;
+    case "**":
+      powOfNumbers(firstNumber, secondNumber);
+      break;
+    case "max":
+      maxOfNumbers(firstNumber, secondNumber);
+      break;
+    default:
+      alert("С таким оператором нет действия.");
+  }
 }
 
 function additionOfNumbers(a, b) {
@@ -73,5 +91,23 @@ function powOfNumbers(a, b) {
 function maxOfNumbers(a, b) {
   const result = Math.max(a, b);
   alert(`Максимальное число: ${result}`);
+  return result;
+}
+
+function cosOfNumber(a) {
+  const result = Math.cos(a);
+  alert(`Косинус числа ${a} равен ${result}`);
+  return result;
+}
+
+function sinOfNumber(a) {
+  const result = Math.sin(a);
+  alert(`Синус числа ${a} равен ${result}`);
+  return result;
+}
+
+function tanOfNumber(a) {
+  const result = Math.tan(a);
+  alert(`Тангенс числа ${a} равен ${result}`);
   return result;
 }
