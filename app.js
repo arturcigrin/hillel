b7f373460e687e8d9577cd71d8c47e701e1e13d3
Chigrin Artur <arturcigrin@gmail.com>
2020-04-28 17:20:34 +0300

    lesson5 - Mathematics

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let quantityOperands = getQuantityOfOperands();
 const operands = getValueOperands();
 showToScreen(operation);
 
-// Cпрашиваем у пользователя какую операцию он хочет выполнить.
 function getOperation() {
   let answerUser;
 
@@ -14,85 +13,85 @@ function getOperation() {
     * - умножение.
     / - деление.
     `);
-  } while (!correctValue(answerUser));
+  } while (!correctOperation(answerUser));
 
   return answerUser;
 }
-/*
- Проверяем, что операция который ввел пользователь нам подходит.
-Ecли ввел нужное значение, то функция возвращает его, если нет то undenfiend...
-*/
-function correctValue(value) {
-  switch (value) {
-    case "+":
-    case "-":
-    case "*":
-    case "/":
-      return value;
-  }
+
+function correctOperation(actionSelectedByUser) {
+  return (
+    actionSelectedByUser === "+" ||
+    actionSelectedByUser === "-" ||
+    actionSelectedByUser === "*" ||
+    actionSelectedByUser === "/"
+  );
 }
-// Спрашиваем у пользователя кол-во операндов.
+
 function getQuantityOfOperands() {
   let answerUser;
+
   do {
     answerUser = +prompt(`Какое кол-во операндов вы хотите использовать?`);
+
     if (answerUser > 1) {
       return answerUser;
     }
+
   } while (true);
 }
 
-// Cпрашиваем у пользователя каждый операнд.
 function getValueOperands() {
-  const operands = [];
+  let userAnswer, valueOfOperands;
+
   do {
-    operand = prompt(`Введите значение ${operands.length + 1}-го операнда`);
-  } while (typeCheckOfNumbers(operand));
+    userAnswer = prompt("Введите  значения через пробел.");
+    valueOfOperands = typeCheckOfNumbers(userAnswer);
+  } while (!valueOfOperands);
 
-  /*
-Проверяем . что вернул в ответе пользователь, и если нужный нам тип то уменьшаем кол-во операндов
-и заносим их в массив.
-P.s Функцию создал здесь т.к. хотел замкнуть массив.
-*/
-  function typeCheckOfNumbers(value) {
-    // Проверяем если пользователь ничего не ввел и нажал OK или Отмена. Т.к. при привединии типов '' и null == 0;
-    switch (value) {
-      case "":
-      case null:
-        return quantityOperands;
-    }
-
-    //   Приводим к числу значение
-    value = Number(value);
-
-    //  Проверяем если это число - уменьшаем кол-во операндов;
-    switch (isNaN(value)) {
-      case true:
-        return quantityOperands;
-    }
-
-    // Проверяем, можно делить на 0 или нет.
-    if (operation === "/" && operands.length > 0 && value === 0) {
-      alert("На 0 делить нельзя! Введите значение >0 || <0");
-      return quantityOperands;
-    }
-
-    operands.push(value);
-
-    return --quantityOperands;
-  }
-
-  return operands;
+  return valueOfOperands;
 }
 
-// Отображаем овтет пользователю
-function showToScreen(action) {
-  switch (action) {
+function typeCheckOfNumbers(numbersEnteredByUser) {
+  if (!numbersEnteredByUser) {
+    alert("Введите значения");
+    return false;
+  }
+
+  let arrOperands = numbersEnteredByUser.split(" ")
+    .filter((isNumber) => +isNumber || +isNumber === 0)
+    .map((number) => +number);
+
+  return divisionByZero(operation, arrOperands) && checkQuantityOperands(arrOperands, quantityOperands);
+
+}
+
+function checkQuantityOperands(arrNumbers, quantityNumbers) {
+  if (arrNumbers.length !== quantityNumbers) {
+    alert(`Вы ввели неверное количество чисел. Вам нужно ввести ${quantityNumbers}`);
+    return false;
+  }
+
+  return arrNumbers;
+}
+
+function divisionByZero(actionUser, arrayValues) {
+  const zeroValues = arrayValues.slice(1).filter((values) => !values);
+
+  if (actionUser === "/" && zeroValues.length) {
+    alert("На 0 делить нельзя");
+    return false;
+  }
+
+  return true;
+}
+
+function showToScreen(actionUser) {
+  switch (actionUser) {
     case "+":
       sum(operands);
       break;
     case "-":
-      min(operands);
+      sub(operands);
       break;
     case "*":
       mult(operands);
@@ -103,56 +102,50 @@ function showToScreen(action) {
   }
 }
 
-function sum(values) {
-  let result = values[0];
-  let str = "";
+function sum(arrayValues) {
+  let strValue = arrayValues[0];
 
-  for (let i = 1; i < values.length; i++) {
-    result += values[i];
-    str += ` + ${values[i]}`;
-  }
+  const result = arrayValues.reduce((acum, value) => {
+    strValue += ` + ${value}`;
+    return (acum += value);
+  });
 
-  alert(values[0] + `${str} = ${result}`);
+  alert(`${strValue} = ${result}`);
   return result;
 }
 
-function min(values) {
-  let result = values[0];
-  let str = "";
+function sub(arrayValues) {
+  let strValue = arrayValues[0];
 
-  for (let i = 1; i < values.length; i++) {
-    result -= values[i];
-    str += ` - ${values[i]}`;
-  }
+  const result = arrayValues.reduce((acum, value) => {
+    strValue += ` - ${value}`;
+    return (acum -= value);
+  });
 
-  alert(values[0] + `${str} = ${result}`);
+  alert(`${strValue} = ${result}`);
   return result;
 }
 
-function mult(values) {
-  let result = values[0];
-  let str = "";
+function mult(arrayValues) {
+  let strValue = arrayValues[0];
 
-  for (let i = 1; i < values.length; i++) {
-    result *= values[i];
-    str += ` * ${values[i]}`;
-  }
+  const result = arrayValues.reduce((acum, value) => {
+    strValue += ` * ${value}`;
+    return (acum *= value);
+  });
 
-  alert(values[0] + `${str} = ${result}`);
+  alert(`${strValue} = ${result}`);
   return result;
 }
 
-function div(values) {
-  let result = values[0];
-  let str = "";
+function div(arrayValues) {
+  let strValue = arrayValues[0];
 
-  for (let i = 1; i < values.length; i++) {
-    result /= values[i];
-    str += ` / ${values[i]}`;
-  }
+  const result = arrayValues.reduce((acum, value) => {
+    strValue += ` / ${value}`;
+    return (acum /= value);
+  });
 
-  result = Number(result.toFixed(2));
-
-  alert(values[0] + `${str} = ${result}`);
+  alert(`${strValue} = ${result}`);
   return result;
 }
