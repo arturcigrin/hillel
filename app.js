93a0028ae03eeedeb6916e0ac6f518ad94395969
Chigrin Artur <arturcigrin@gmail.com>
2020-05-18 09:48:16 +0300

    add task - calculate

--- app.js
+++ app.js
@@ -1,37 +1,103 @@
-const btnAddTask = document.querySelector("#formTask button");
-const inputFormTask = document.querySelector("#formTask input");
-const todoList = document.querySelector("#todoList");
-let countTask = 1;
+const firstArgument = document.querySelector('#firstArgument');
+const secondArgument = document.querySelector('#secondArgument');
+const operations = document.querySelector('#operations')
+const btnCalculate = document.querySelector('#calculate');
+const blockResult = document.querySelector('.result');
 
-inputFormTask.addEventListener("input", validationInput);
-btnAddTask.addEventListener("click", onAddTask);
+btnCalculate.addEventListener("click", onCalculate);
 
-function onAddTask(event) {
+function onCalculate(event) {
   event.preventDefault();
+  const listArguments = getValueInputs(firstArgument, secondArgument);
+  const userAction = getUserAction(operations);
 
-  const valueInput = getValueInput(inputFormTask);
-  const li = createElementLi(valueInput);
-  renderTask(todoList, li);
+  clearInput(firstArgument, secondArgument);
+
+  chekWhatToShow(listArguments, userAction) ?
+    showResult(blockResult, listArguments, userAction) :
+    showMessageError(blockResult);
 }
 
-function getValueInput(inputElement) {
-  const value = inputElement.value;
-  inputElement.value = "";
-  validationInput.call(inputElement);
-  return value;
+function getValueInputs(firstInput, secondInput) {
+  return filterAndCheckValues(firstInput.value, secondInput.value);
 }
 
-function createElementLi(textTask) {
-  const li = document.createElement("li");
-  li.innerText = `${countTask++} ${textTask}`;
-
-  return li;
+function filterAndCheckValues(...listValues) {
+  return listValues.filter(value => !isNaN(value) && value.trim()).map(number => +number);
 }
 
-function renderTask(containerTasks, task) {
-  containerTasks.append(task);
+function getUserAction(selectElement) {
+  return selectElement.value;
 }
 
-function validationInput() {
-  this.value.trim() ? btnAddTask.disabled = false : btnAddTask.disabled = true;
+function getResult(operation, arrayValues) {
+  switch (operation) {
+    case '+':
+      return sum(arrayValues);
+    case '-':
+      return sub(arrayValues);
+    case '*':
+      return mult(arrayValues);
+    case '/':
+      return div(arrayValues);
+    case 'min':
+      return min(arrayValues);
+    case 'max':
+      return max(arrayValues);
+  }
+}
+
+function showResult(elementResult, arrayArguments, operationSelectedByUser) {
+
+  const result = getResult(operationSelectedByUser, arrayArguments);
+
+  elementResult.innerText = `Результат: ${result}`;
+
+  if (elementResult.classList.contains('hidden')) {
+    elementResult.classList.remove('hidden');
+  }
+}
+
+function showMessageError(elementResult) {
+  elementResult.innerText = 'Ошибка: вы не ввели число или поделили на ноль';
+
+  if (elementResult.classList.contains('hidden')) {
+    elementResult.classList.remove('hidden');
+  }
+}
+
+function clearInput(...inputs) {
+  inputs.forEach(input => input.value = '');
+}
+
+function checkDivisionByZero(secondArgument, operation) {
+  return operation === '/' && secondArgument === 0 ? false : true;
+}
+
+function chekWhatToShow(arrayArguments, operationSelectedByUser) {
+  return checkDivisionByZero(arrayArguments[1], operationSelectedByUser) && arrayArguments.length > 1 ? true : false;
+}
+
+function sum(listArguments) {
+  return listArguments.reduce((result, value) => result += value);
+}
+
+function sub(listArguments) {
+  return listArguments.reduce((result, value) => result -= value);
+}
+
+function mult(listArguments) {
+  return listArguments.reduce((result, value) => result *= value);
+}
+
+function div(listArguments) {
+  return listArguments.reduce((result, value) => result /= value);
+}
+
+function min(listArguments) {
+  return Math.min(...listArguments);
+}
+
+function max(listArguments) {
+  return Math.max(...listArguments);
 }
