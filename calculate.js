7b883f1359c908b9270ce372ccba2c357aeb0a3a
Chigrin Artur <arturcigrin@gmail.com>
2020-07-13 09:35:54 +0300

    Calculate NodeJS

---
+++ calculate.js
@@ -0,0 +1,35 @@
+function calculate() {
+  let _arg = 0;
+
+  return {
+    set setValue(arg1) {
+      _arg = arg1;
+    },
+
+    printResult() {
+      console.log(_arg);
+      return this;
+    },
+    sum(arg2 = 0) {
+      _arg += arg2;
+      return this;
+    },
+
+    sub(arg2 = 0) {
+      _arg -= arg2;
+      return this;
+    },
+
+    mult(arg2) {
+      _arg *= arg2;
+      return this;
+    },
+
+    div(arg2 = 1) {
+      _arg /= arg2;
+      return this;
+    },
+  };
+}
+
+module.exports = calculate();
